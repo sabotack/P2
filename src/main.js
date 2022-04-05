2083b47852a86e6beeebc74a0463b7774e27329c
sabotack <sabotack.2001@gmail.com>
2022-04-05 09:31:24 +0200

    Initial file server setup
    Co-authored-by: sBejer <sBejer@users.noreply.github.com>
    Co-authored-by: Emil Monrad Laursen <EmilML@users.noreply.github.com>
    Co-authored-by: Viktor Platz <redviktor@users.noreply.github.com>
    Co-authored-by: AlexanderManich <AlexanderManich@users.noreply.github.com>
    Co-authored-by: Esben <EMojoKrogh@users.noreply.github.com>
    Co-authored-by: AndersToft20 <AndersToft20@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,14 +1,65 @@
 import http from 'http';
+import fs from 'fs';
+import path from 'path';
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('\n')
-})
+const publicResources = "./public";
 
-server.listen(port, hostname, () => {
+const server = http.createServer((req, res) => {
+    let filePath = publicResources + req.url;
+    
+    if (req.url === '/') {
+        filePath = publicResources + '/index.html';
+    }
+
+    let contentType = getContentType(filePath);
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                fs.readFile(publicResources + '/404.html', function(error, content) {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            }
+            else {
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+    
+}).listen(port, hostname, () => {
     console.log(`Server is running at http://${hostname}:${port}/`)
 })
+
+
+function getContentType(fPath) {
+    let extname = String(path.extname(fPath)).toLowerCase();
+    let mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+    return mimeTypes[extname] || 'application/octet-stream';
+}
+
