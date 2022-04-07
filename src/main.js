1be6ff17d01d3f3c528d1cb4ffcf76ac0fd1e10d
Simon Mikkelsen Bejer <simonbejer@h312.aau1x-2.klient.str.site.aau.dk>
2022-04-07 15:38:01 +0200

    Google auth. Front end og server

--- src/main.js
+++ src/main.js
@@ -1,18 +1,116 @@
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
+import querystring from 'querystring';
+
+
+//Google authentication stuff
+import { OAuth2Client } from 'google-auth-library'; //Contructor function from google
+const GOOGLE_CLIENT_ID = `949751936924-lm4a4d4kqhv01671h3dihu6gcstjsmv4.apps.googleusercontent.com`; //Google client
+const googleClient = new OAuth2Client(`${GOOGLE_CLIENT_ID}`);
+
 
 const hostname = 'localhost';
 const port = 3000;
-
 const publicResources = "./public";
 
-const server = http.createServer((req, res) => {
-    let filePath = publicResources + req.url;
+
+const server = http.createServer(function(request, response){
     
-    if (req.url === '/') {
-        filePath = publicResources + '/index.html';
+    try{
+        processUserRequest(request, response);
+    }catch(e){
+        console.log("Server error"+e);
+        errorResponseUser(request, response);
     }
+    
+});
+
+server.listen(port, function(){
+    console.log(`Server running on http://${hostname}:${port}/`);
+});
+
+function processUserRequest(request, response){
+
+    console.log("GOT: " + request.method + " " + request.url);
+    let requestMethod = request.method.toLowerCase();
+    console.log(requestMethod)
+    
+    let filePath = publicResources + request.url;
+    console.log(request.url);
+
+    switch(requestMethod){
+        case 'get':
+            switch(request.url){
+                case `/`:
+                    console.log("/ case");
+                    filePath = publicResources + '/index.html';
+                    readFile(filePath, request, response);
+                    break;
+                case `/index.html`:
+                    console.log("index case");
+                    readFile(filePath, request, response);
+                    break;
+                case '/login.css':
+                    console.log("css case");
+                    readFile(filePath, request, response);
+                    break;
+                case '/googleAuth.js':
+                    console.log("googleAuth case");
+                    readFile(filePath, request, response);
+                    break;
+            }
+        case 'post':
+            switch(request.url){
+                case `/message`:
+                    console.log("POST CASE MESSAGE");
+                    
+                    let requestBody = '';
+                    request.on('data', data => {
+                        
+                        if (requestBody.length > 1e6) { 
+                            alert("Too much");
+                            request.connection.destroy();
+                        }
+
+                        requestBody += data;
+
+                    })
+
+                    request.on('end', () => {
+                        let parsedData = querystring.decode(requestBody);
+                        console.log(parsedData);
+
+                        response.writeHead(200, "OK", {'Content-Type':'text/plain'});
+                        response.write('The POST output response: \n\n');
+                        response.write(requestBody);
+                        response.end("\n\nEnd of message to browser");
+                    })
+
+                    break;
+                case `/login`:
+                    handleGoogleToken(request, response);
+                    break;
+            }
+    }
+
+}
+
+function validatePOST(request){
+
+    console.log(smt)
+}
+
+function errorResponseUser(request, response){
+
+    response.statusCode = 500;
+    response.writeHead(500, "ERROR", {"Content-Type":"text/plain"});
+    response.write("Internal server error");
+    response.end();
+
+}
+
+function readFile(filePath, request, response){
 
     let contentType = getContentType(filePath);
 
@@ -20,25 +118,22 @@ const server = http.createServer((req, res) => {
         if (error) {
             if(error.code == 'ENOENT') {
                 fs.readFile(publicResources + '/404.html', function(error, content) {
-                    res.writeHead(404, { 'Content-Type': 'text/html' });
-                    res.end(content, 'utf-8');
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
                 });
             }
             else {
-                res.writeHead(500);
-                res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
             }
         }
         else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
         }
     });
-    
-}).listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}/`)
-})
 
+}
 
 function getContentType(fPath) {
     let extname = String(path.extname(fPath)).toLowerCase();
@@ -63,3 +158,32 @@ function getContentType(fPath) {
     return mimeTypes[extname] || 'application/octet-stream';
 }
 
+function handleGoogleToken(request, response) {
+    let token = '';
+    request.on('data', data => {
+        if (token.length > 1e6) { 
+            alert("Too much");
+            request.connection.destroy();
+        }
+        token += data;
+        verifyGoogleAccount(token).catch(console.error);
+    })
+
+    request.on('end',() => {
+        console.log("Login accessed");
+        response.writeHead(200, "OK", {'Content-Type':'text/plain'});
+        response.write('The POST output response: \n\n');
+        response.write(token);
+        response.end("\n\nEnd");
+    })
+}
+
+async function verifyGoogleAccount(token) {
+    const ticket = await googleClient.verifyIdToken({
+        idToken: token,
+        audience: GOOGLE_CLIENT_ID
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    console.log(payload);
+}
