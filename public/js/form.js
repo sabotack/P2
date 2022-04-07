ff475b0fafb1795b6620ecc2fd1379eb6d2933ba
Viktor Platz <redviktor10@hotmail.com>
2022-04-07 23:00:21 +0200

    Check for input API  call to event

--- public/js/form.js
+++ public/js/form.js
@@ -1,6 +1,16 @@
 let addTransportButtons = document.querySelectorAll('.transport-button');
 let addToBtn = addTransportButtons[0], addFromBtn = addTransportButtons[1];
 
+let eventLocation = document.getElementById('eventlocation');
+let eventTime = document.getElementById('starttime');
+let eventDate = document.getElementById('startdate');
+
+let transportToEventFieldInput = false;
+let transportFromEventFieldInput = false;
+let eventDateInput = false;
+let eventTimeInput = false;
+let eventLocationInput = false;
+
 addToBtn.addEventListener('click', () => {
     addToBtn.innerHTML = "";
     let location = document.createElement('input');
@@ -8,12 +18,11 @@ addToBtn.addEventListener('click', () => {
     setAttributes(location, attributes);
     
     addToBtn.appendChild(location);  
-    console.log("NEW TEXT BOX");
     let transportToEventField = document.getElementById('startlocation');
 
-    transportToEventField.addEventListener('keydown', function (evt) {
-        console.log("INPUT ON TRANSPROT TO EVENT FIELD")
-    });
+    transportToEventField.addEventListener('input', function (evt) {
+        transportToEventFieldInput = true;
+    }, { once: true });
     
 }, { once: true });
 
@@ -24,17 +33,39 @@ addFromBtn.addEventListener('click', () => {
     setAttributes(location, attributes);
     
     addFromBtn.appendChild(location);  
-    console.log("NEW TEXT BOX");
     let transportFromEventField = document.getElementById(`endlocation`);
 
-    transportFromEventField.addEventListener('keydown', function (evt) {
-        console.log("INPUT ON TRANSPROT FROM EVENT FIELD");
-    });
+    transportFromEventField.addEventListener('input', function (evt) {
+        transportFromEventFieldInput = true;
+    }, { once: true });
 }, { once: true });
 
 
 function setAttributes(el, attrs) {
     for(let key in attrs) {
         el.setAttribute(key, attrs[key]);
+    }
+}
+
+eventDate.addEventListener('input', function (evt) {
+    eventDateInput = true;
+    checkIfFieldsAreFilled();
+}, { once: true });
+
+eventTime.addEventListener('input', function (evt) {
+    eventTimeInput = true;
+    checkIfFieldsAreFilled();
+}, { once: true });
+
+eventLocation.addEventListener('input', function (evt) {
+    eventLocationInput = true;
+    checkIfFieldsAreFilled();
+}, { once: true });
+
+function checkIfFieldsAreFilled(){
+    if (transportToEventFieldInput && eventDateInput && eventTimeInput && eventLocationInput){
+
+        console.log("All required fields are filled for transportToEventAPIcall");
+    
     }
 }
