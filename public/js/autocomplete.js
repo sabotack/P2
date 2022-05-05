e16e73cb88a9bdfa5a85a987f7013aeb7b7004b0
Viktor Platz <redviktor10@hotmail.com>
2022-05-05 13:53:09 +0200

    Format write

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -45,8 +45,8 @@ function autocomplete(input) {
         list.setAttribute('id', input.id + 'autocomplete-list');
         list.setAttribute('class', 'autocomplete-items');
         input.parentNode.appendChild(list);
-        locationServiceCallAPI(input.value).then(
-            (response) => {
+        locationServiceCallAPI(input.value)
+            .then((response) => {
                 response.forEach((element) => {
                     let option = document.createElement('div');
                     option.innerHTML += element[':@']['@_name'];
@@ -121,9 +121,8 @@ function autocomplete(input) {
 
                     list.appendChild(option);
                 });
-            }
-        )
-        .catch(error => console.log(error));
+            })
+            .catch((error) => console.log(error));
     });
 }
 
--- public/js/rejseplanen.js
+++ public/js/rejseplanen.js
@@ -4,58 +4,59 @@ function locationServiceCallAPI(inputField) {
     return fetch('http://localhost:3000/locationService', {
         //returns promise, and then proceeds to fetch server.
         method: 'POST',
-        headers: { 'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-        body: ('location=' + inputField) //body contains stringified events in order to send accross network
+        headers: { 'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+        body: 'location=' + inputField //body contains stringified events in order to send accross network
     })
-    .then((response) => {
-        if(!response.ok) {
-            const e = new Error(response.statusText);
-            e.code = response.status;
-            throw e;
-        }
-        //when response is recieved, convert it to json.
-        return response.json();
-    })
-    .catch((error) => { 
-        return Promise.reject(error);
-    });
+        .then((response) => {
+            if (!response.ok) {
+                const e = new Error(response.statusText);
+                e.code = response.status;
+                throw e;
+            }
+            //when response is recieved, convert it to json.
+            return response.json();
+        })
+        .catch((error) => {
+            return Promise.reject(error);
+        });
 }
 
 function tripServiceCallAPI(tripData) {
     return fetch('http://localhost:3000/tripService', {
         //returns promise, and then proceeds to fetch server.
         method: 'POST',
-        headers: { 'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-        body: ('originCoordName=' +
-        tripData.originCoordName +
-        '&originCoordX=' +
-        tripData.originCoordX +
-        '&originCoordY=' +
-        tripData.originCoordY +
-        '&destCoordX=' +
-        tripData.destCoordX +
-        '&destCoordY=' +
-        tripData.destCoordY +
-        '&destCoordName=' +
-        tripData.destCoordName +
-        '&date=' +
-        tripData.date +
-        '&time=' +
-        tripData.time +
-        '&searchForArrival=' +
-        tripData.searchForArrival) //body contains stringified events in order to send accross network
+        headers: { 'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+        body:
+            'originCoordName=' +
+            tripData.originCoordName +
+            '&originCoordX=' +
+            tripData.originCoordX +
+            '&originCoordY=' +
+            tripData.originCoordY +
+            '&destCoordX=' +
+            tripData.destCoordX +
+            '&destCoordY=' +
+            tripData.destCoordY +
+            '&destCoordName=' +
+            tripData.destCoordName +
+            '&date=' +
+            tripData.date +
+            '&time=' +
+            tripData.time +
+            '&searchForArrival=' +
+            tripData.searchForArrival //body contains stringified events in order to send accross network
     })
-    .then((response) => {
-        if(!response.ok) {
-            const e = new Error(response.statusText);
-            e.code = response.status;
-            throw e;
-        }
+        .then((response) => {
+            if (!response.ok) {
+                const e = new Error(response.statusText);
+                e.code = response.status;
+                throw e;
+            }
 
-        //when response is recieved, convert it to json.
-        return response.json();
-    })
-    .catch((error) => {
-        return Promise.reject(error);
-    });
+            //when response is recieved, convert it to json.
+            return response.json();
+        })
+        .catch((error) => {
+            return Promise.reject(error);
+        });
 }
