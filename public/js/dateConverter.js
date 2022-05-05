d47ab3a363fc48ffad8254cd8dd2716486af38e2
barth010may <borup.rasmus@gmail.com>
2022-05-05 23:45:53 +0200

    yall motherfuckers got pranked bitch

--- public/js/dateConverter.js
+++
@@ -1,7 +0,0 @@
-export { convertToDate };
-
-/* Converts DD.MM.YY to YYYY-MM-DD */
-function convertToDate(date) {
-    let dateParts = date.split('.');
-    return '20' + dateParts[2] + '-' + dateParts[1] + '-' + dateParts[0];
-}
